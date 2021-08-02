a5026ac959f9823a6354c97d958c837d75e4b334
Xengie Cheng DOAN <xengie.doan@mp0751.uni.lux>
2021-08-02 13:58:06 +0200

    returns all items and edited data extracted

--- zotero.py
+++ zotero.py
@@ -1,6 +1,9 @@
 from pyzotero import zotero
 import pandas as pd
 import re
+from configparser import ConfigParser
+from rich.console import Console
+from sys import path as syspath
 
 def list_strip(lst: list) -> list:
     for i in range(len(lst)):
@@ -23,29 +26,54 @@ def extrtact_data(df: pd.DataFrame) -> pd.DataFrame:
     # df = df[df['itemType'] != 'attachment'].copy()
     df['authors'] = [ creators_to_names(i) for i in df.creators]
     df['year'] = df.date.apply( lambda x: re.search(r'([0-9]{4})', str(x)).group(1) if re.search(r'([0-9]{4})', str(x)) else '*' )
-    df['author_year'] = df.authors.apply(lambda x: ', '.join(x) if len(x) <= 2 else ', '.join(x[:2]) + ' ...')
-    df['author_year'] = '(' + df['author_year']  + ', ' +  df['year']  + ')'
-    data = df[df['itemType'] != 'attachment'][['author_year', 'title', 'authors', 'year', 'itemType', 'key', 'version']].T.to_dict()
+    # df['author_year'] = df.authors.apply(lambda x: ', '.join(x) if len(x) <= 2 else ', '.join(x[:2]) + ' ...')
+    # df['author_year'] = '(' + df['author_year']  + ', ' +  df['year']  + ')'
+    data = df[df['itemType'] != 'attachment'][['title', 'authors', 'year', 'itemType', 'key', 'version']].T.to_dict()
     return data
 
 def fetch_zotero_items(LIBRARY_ID: int, LIBRARY_TYPE: str, API_KEY: str) -> pd.DataFrame:
     lib = zotero.Zotero(LIBRARY_ID, LIBRARY_TYPE, API_KEY)
-    items = lib.items()
+    items = lib.everything(lib.items())
     try:
         df = pd.DataFrame(pd.DataFrame(items)['data'].to_list())
+        # print(df)
+        return df
     except Exception as e:
         print('Error:' + str(e))
-    return df
-
-
-# if __name__ == '__main__':
     
-#     try:
-#         print('Start running...')
-#         df = fetch_zotero_items(LIBRARY_ID, LIBRARY_TYPE, API_KEY)
-#         data = extrtact_data(df)
-#         for item in data.items():
-#             print(item[1]['title'])
-#     except KeyboardInterrupt as k:
-#         print('\nKey pressed to interrupt...')
+
+# testing 
+# if __name__ == '__main__':
+## GET CONFIGURATION ###############################################################################
+# GLOBAL VARS #
+# CONSOLE = Console()
+# CLIENT, PAGE, CV = None, None, None
+
+
+# try:
+#     cfg = ConfigParser()
+#     cfg.read(syspath[0] + '/config.ini')
+#     PAGE_URL = cfg.get('notion', 'PAGE_URL')
+#     CV_URL = cfg.get('notion', 'CV_URL')
+#     TOKEN_V2 = cfg.get('notion', 'TOKEN_V2')
+#     LIBRARY_ID = int(cfg.get('zotero', 'LIBRARY_ID'))
+#     LIBRARY_TYPE = cfg.get('zotero', 'LIBRARY_TYPE')
+#     API_KEY = cfg.get('zotero', 'API_KEY')
+
+# except Exception as e:
+#     CONSOLE.print('Config file error, exit...', style='bold red')
+#     exit()
+
+
+# try:
+#     print('Start running...')
+#     df = fetch_zotero_items(LIBRARY_ID, LIBRARY_TYPE, API_KEY)
+#     # print(df)
+    
+#     data = extrtact_data(df)
+#     print(data)
+#     for item in data.items():
+#         print(item[1]['title'])
+# except KeyboardInterrupt as k:
+#     print('\nKey pressed to interrupt...')
 
